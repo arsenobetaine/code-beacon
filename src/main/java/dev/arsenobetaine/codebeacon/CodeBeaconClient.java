a3cb7ba87957a208b27cff24c6707fdb0bdc57bb
Sahand Soleimani <arsenobetaine@gmail.com>
2025-04-06 20:19:23 +0330

    Successful Initial Setup

---
+++ src/main/java/dev/arsenobetaine/codebeacon/CodeBeaconClient.java
@@ -0,0 +1,10 @@
+package dev.arsenobetaine.codebeacon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CodeBeaconClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
