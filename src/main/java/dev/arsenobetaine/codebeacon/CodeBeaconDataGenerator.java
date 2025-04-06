19cbf808731fb81e6b8f28f2dd744c562336532f
Sahand Soleimani <arsenobetaine@gmail.com>
2025-04-06 19:36:35 +0330

    Initial commit

---
+++ src/main/java/dev/arsenobetaine/codebeacon/CodeBeaconDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.arsenobetaine.codebeacon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CodeBeaconDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
